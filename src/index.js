008a13b733559c576848fc8cb74be4561ef7d14a
Grace <gcw.chung@gmail.com>
2020-08-27 02:13:54 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import weather from 'weather-gov-api';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            lat: null, 
+            lon: null,
+            forecast: '',
+            degree: null,
+            sfLat: 37.7749,
+            sfLon: -122.4194,
+            icon: '',
+        };
+    }
+    
+    componentDidMount() {
+        // Geolocation API
+        window.navigator.geolocation.getCurrentPosition(
+            position => { 
+                this.setState({ 
+                lat: position.coords.latitude,
+                lon: position.coords.longitude
+                });
+            
+            const data = {
+                lat: position.coords.latitude,
+                lon: position.coords.longitude
+            }
+
+            // Weather API
+            weather.getForecast('default', data.lat, data.lon)
+            .then(data => { this.setState({
+                degree: data.data.properties.periods[0].temperature,
+                icon: data.data.properties.periods[0].icon
+                }) 
+            })
+            .catch(err => console.log(err));
+            })
+    }
+         
+    render() {
+       return(
+           <div>
+               <h1>Simple Weather App</h1>
+              
+                <div className="location">
+                <h3>Pick Location</h3>
+                <div className="latlon">
+                <div className="lat" ><span style={{fontSize: 10, color:"gray"}}>Lat</span>
+                    <div style={{color: "lightblue", fontWeight: 700}}>{this.state.lat}</div>
+                </div>
+
+                <div className="lon" ><span style={{fontSize: 10, color:"gray"}}>Long</span>
+                    <div style={{color: "lightblue", fontWeight: 700}}>{this.state.lon}</div>
+                </div>
+                </div>
+                </div>
+
+                <div>
+                <h3>Weather</h3>
+                    <span className="temp">{this.state.degree}°</span>
+                    <img src={this.state.icon} />
+                </div>
+
+           </div>
+       );
+
+    }
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
